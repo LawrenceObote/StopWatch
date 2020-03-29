76ccedf2693e37a873ee077f9d5253633835ab5b
Lawrence Obote <dev.obote@gmail.com>
2020-03-29 14:39:37 -0400

    Starting Again from the beginning with the help of a tutorial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react'
-import './App.css'
-// import StopWatch from './StopWatch'
-import TimerControl from './TimerControl'
+import React, { Component } from "react";
 
+import TimerControl from "./TimerControl";
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    
-    this.state = {
-      isPaused: false,
-      timer: 0
-    }
-  }
-  
-  render(){
-  
+  render() {
     return (
       <div className="App">
-        {/* <StopWatch></div> */}
-        <TimerControl></TimerControl>
+        <div className ="App-title">Timer Demo</div>
+        <div className="Timer">
+          <TimerControl/>
+        </div>
       </div>
-    )
+    );
   }
 }
 
 export default App;
+
+
+
+// import React, { Component } from 'react'
+// import './App.css'
+// // import StopWatch from './StopWatch'
+// import TimerControl from './TimerControl'
+
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+    
+//     this.state = {
+//       isPaused: false,
+//       timer: 0
+//     }
+//   }
+  
+//   render(){
+  
+//     return (
+//       <div className="App">
+//         {/* <StopWatch></div> */}
+//         <TimerControl></TimerControl>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
--- src/TimerControl.js
+++ src/TimerControl.js
@@ -1,47 +1,86 @@
-
-import React, { Component } from 'react'
-
 class TimerControl extends Component {
-    constructor(props) {
-        super(props);
-        this.handleStart = this.handleStart.bind(this);
-        this.handlePause = this.handlePause.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-        this.state = {isPaused: false};
-        this.state = {timer: 0};
-    }
-
-    handleStart(){
-        this.setState({isPaused: false});
-        setInterval(this.setState(state => ({
-            timer: this.state.timer + 1
-        })), 1000)
- 
-
-    }
-    handlePause (){
-        this.setState({isPaused: true});
-        
-    }
-    handleReset (){
-        this.setState({timer: 0})
-    }
-
-    render(){
-        const {timer} = this.state
+    render() {
         return (
-            <div>
-                <h1>{timer}</h1>
-                <button onClick={this.handleStart}>Start</button>
-                <button onClick={this.handlePause}>Pause</button>
-                <button onClick={this.handleReset}>Reset</button>
+            <div className="TimerControl">
+                <div className="Timer-header">Stopwatch</div>
             </div>
-        )
+        );
     }
-    componentDidMount(){
-        let time = this.state.timer;
-        this.setState({timer: time += 1})
-    }
+}
+// import React, { Component } from 'react'
+
+// class TimerControl extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleStart = this.handleStart.bind(this);
+//         this.handlePause = this.handlePause.bind(this);
+//         this.handleReset = this.handleReset.bind(this);
+//         this.state = {isPaused: false};
+//         this.state = {timer: 0};
+//     }
+
+//     handleStart(){
+        // this.setState({
+        //     timer: this.state.timer
+        // })
+        // this.timer = setInterval(() => this.setState({
+        //     timer: Date.now() - this.state.timer
+        // }), 1);
+//         let time = this.state.timer
+//         this.setState({timer: setInterval(function (){time += 1}, 1000)})
+       
+
+//     }
+//     handlePause (){
+//         this.setState({isPaused: true});
+        
+//     }
+//     handleReset (){
+//         this.setState({timer: 0})
+//     }
+
+//     render(){
+//         const {timer} = this.state
+//         return (
+//             <div>
+//                 <h1>{timer}</h1>
+//                 <button onClick={this.handleStart}>Start</button>
+//                 <button onClick={this.handlePause}>Pause</button>
+//                 <button onClick={this.handleReset}>Reset</button>
+//             </div>
+//         )
+//     }
+//     componentDidMount(){
+//         let time = this.state.timer
+//         if(isPaused){
+//             this.interval = setInterval(() =>{
+//                 this.setState({timer: time += 1 })
+//             }, 1000)
+//         }
+//     }
+    // componentDidMount () {
+    //     const {time} = this.props
+    //     this.setState({
+    //         timer: time
+    //     })
+    //     this.myInterval = setInterval(() => {
+    //         this.setState(prevState => ({
+    //            timer: prevState.count + 1 
+    //         }))
+    //     }, 1000)
+
+    // }
+    // componentDidMount( function(){
+    //     this.setInterval( () =>{
+    //         this.setState({
+    //            timer: this.state.timer + 1 
+    //         })
+    //     }, 1000);
+    // });
+    // componentDidMount(){
+    //     let time = this.state.timer;
+    //     this.setState({timer: setInterval(function (){time += 1}, 1000)})
+    // }
     // componentDidMount() {
     //     this.interval = setInterval(() => this.setState({ timer: this.state += 1 }), 1000);
     //   }
@@ -56,6 +95,6 @@ class TimerControl extends Component {
     //         }))
     //     }, 1000)
     // }
-}
+// }
 
-export default TimerControl
+export default TimerControl;
